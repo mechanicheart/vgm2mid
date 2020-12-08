d25aef6ce5eb9109e488e3cb2174321aa10539ab
mechanicheart <34300844+mechanicheart@users.noreply.github.com>
2020-12-08 19:20:23 +0800

    Delete IChip.vb

--- core/mapper/core/chip/IChip.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace Core
-    Public MustInherit Class IChip
-        Protected iMastClock As Integer
-        Protected iSlavClock As Integer
-
-        Public MustOverride Sub Init()
-    End Class
-End Namespace
